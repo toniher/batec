2add23744915f20f221427cabc9fd4f006a374e6
Jordi Mas <jmas@softcatala.org>
2017-08-23 15:51:36 +0200

    First importer

---
+++ data-importers/catalanitzador.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 Jordi Mas i Hernandez <jmas@softcatala.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+import urllib.request, urllib.parse, urllib.error
+from dataimport import DataImport
+
+
+class Catalanitzador(DataImport):
+
+    def obtain_data(self):
+        url = "https://www.softcatala.org/catalanitzador/response.php"
+        print("url->" + url)
+
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        print("Returned data:" + str(data))
+        return data
+
