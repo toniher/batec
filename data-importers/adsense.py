a58b2ad5fc9ecb4e9dcb4335db71c99254a72018
Jordi Mas <jmas@softcatala.org>
2017-10-01 13:29:29 +0200

    Formmating fixes

--- data-importers/adsense.py
+++ data-importers/adsense.py
@@ -23,9 +23,7 @@ from datetime import datetime
 from adsense_util import get_account_id
 from adsense_util_data_collator import DataCollator
 from googleapiclient import discovery
-from oauth2client import client
-from oauth2client import file
-from oauth2client import tools
+from oauth2client import client, file, tools
 import httplib2
 import os
 
@@ -35,11 +33,10 @@ class AdSense(DataImport):
     def init(self, name, version, doc, filename, discovery_filename, scope):
         # Name of a file containing the OAuth 2.0 information details
         client_secrets = os.path.join(os.path.dirname(filename),
-                                    'client_secrets.json')
+                                      'client_secrets.json')
 
         # Set up a Flow object to be used if we need to authenticate.
-        flow = client.flow_from_clientsecrets(client_secrets,
-            scope=scope,
+        flow = client.flow_from_clientsecrets(client_secrets, scope=scope,
             message=tools.message_if_missing(client_secrets))
 
         # Prepare credentials, and authorize HTTP object with them.
@@ -51,8 +48,8 @@ class AdSense(DataImport):
         if credentials is None or credentials.invalid:
             flags = None
             credentials = tools.run_flow(flow, storage, flags)
-        
-        http = credentials.authorize(http = httplib2.Http())
+
+        http = credentials.authorize(http=httplib2.Http())
 
         if discovery_filename is None:
             # Construct a service object via the discovery service.
@@ -65,57 +62,54 @@ class AdSense(DataImport):
                     base='https://www.googleapis.com/',
                     http=http)
         return service
-    
+
     def extract_data(self):
 
-      # Authenticate and construct service.
-      service = self.init(
-          # def init(name, version, doc, filename, discovery_filename=None)
-          'adsense', 'v1.4', __doc__, __file__, None,
+        # Authenticate and construct service.
+        service = self.init('adsense', 'v1.4', __doc__, __file__, None,
           'https://www.googleapis.com/auth/adsense.readonly')
 
-      # Process flags and read their values.
-      saved_report_id = 0
+        # Process flags and read their values.
+        saved_report_id = 0
 
-      try:
-        # Let the user pick account if more than one.
-        account_id = get_account_id(service)
+        try:
+            # Let the user pick account if more than one.
+            account_id = get_account_id(service)
 
-        # Retrieve report.
-        if saved_report_id:
-          result = service.accounts().reports().saved().generate(
-              accountId=account_id, savedReportId=saved_report_id).execute()
-        else:
-          result = service.accounts().reports().generate(
-              accountId=account_id, startDate='today-1d', endDate='today-1d',
-              metric=['PAGE_VIEWS', 'EARNINGS'],
-              dimension=['DATE'],
-              sort=['+DATE']).execute()
+            if saved_report_id:
+                result = service.accounts().reports().saved().generate(
+                    accountId=account_id, savedReportId=saved_report_id).execute()
+            else:
+                result = service.accounts().reports().generate(
+                  accountId=account_id, startDate='today-1d', endDate='today-1d',
+                  metric=['PAGE_VIEWS', 'EARNINGS'],
+                  dimension=['DATE'],
+                  sort=['+DATE']).execute()
 
-        result = DataCollator([result]).collate_data()
+            result = DataCollator([result]).collate_data()
 
-        data = {}
-        for row in result['rows']:
-            for i in range(0, len(row)):
-                header = result['headers'][i]['name'].lower()
-                data[header] = row[i]   
-        return data       
+            data = {}
+            for row in result['rows']:
+                for i in range(0, len(row)):
+                    header = result['headers'][i]['name'].lower()
+                    data[header] = row[i]
 
-      except client.AccessTokenRefreshError:
-        print ('The credentials have been revoked or expired, please re-run the '
-               'application to re-authorize')
+            return data
+
+        except client.AccessTokenRefreshError:
+            print('The credentials have been revoked or expired,'
+                  'please re-run the application to re-authorize')
 
     def transform_data(self, data):
         current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
         json_body = [
             {
-                "time" : current_time,
+                "time": current_time,
                 "measurement": "adsense",
                 "fields": {
-                    "page_views" : data['page_views'],
-                    "earnings" : data['earnings'],
-    
+                    "page_views": data['page_views'],
+                    "earnings": data['earnings'],
                 }
             }
         ]
@@ -125,6 +119,3 @@ class AdSense(DataImport):
         data = self.extract_data()
         json = self.transform_data(data)
         self.load_data(json)
-
-
-
